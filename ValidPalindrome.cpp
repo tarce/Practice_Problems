9a98e3ab8d3bd278bc35b8950e2697e1d754c265
Terek Arce <terek.arce@gmail.com>
2014-10-31 11:03:59 -0400

    Update ValidPalindrome.cpp

--- ValidPalindrome.cpp
+++ ValidPalindrome.cpp
@@ -1,3 +1,10 @@
+/*
+* Given a string, determine if it is a palindrome, 
+* considering only alphanumeric characters and ignoring cases.
+* For the purpose of this problem, we define empty string as
+* valid palindrome.
+*/
+
 class Solution {
 public:
     bool isPalindrome(string s) {
